5d830fb6b04fe9dbffd1e2f97bbb19a4bf6a804b
pallavimunde123 <pallavimunde19@gmail.com>
2026-01-22 15:45:36 +0530

    second

--- src/BasicProgram/Demo.java
+++ src/BasicProgram/Demo.java
@@ -4,5 +4,8 @@ public class Demo {
 public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("My Name is pallavi");
+	
+	System.out.println("heyy ");
+	
 }
 }
