6a0f4db129fec65929e7ae44c1b06491c05a08f0
pallavimunde123 <pallavimunde19@gmail.com>
2026-01-22 15:16:10 +0530

    First Push

---
+++ src/BasicProgram/Demo.java
@@ -0,0 +1,8 @@
+package BasicProgram;
+
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("Hello World");
+	System.out.println("My Name is pallavi");
+}
+}
---
+++ src/SecondProgram/Text.java
@@ -0,0 +1,5 @@
+package SecondProgram;
+
+public class Text {
+
+}
